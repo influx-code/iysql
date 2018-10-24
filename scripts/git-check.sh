2660010b1fba24ec9ca708ef1b4c0dc92983bb7b
Jeffery <jeffery@influx.io>
2018-10-24 11:11:21 +0800

    测试提交

---
+++ scripts/git-check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eu
+
+app='.'
+echo -e '\033[33mstart git push check\033[0m'
+
+echo '清理.DS_Store文件'
+find $app -name ".DS_Store" -print0 | xargs -0 rm -rf  
+git add $app
+echo 'git commit'
+git commit -m $1
+git pull
