cfa202146df8ba0827e200b476829460ff9c75ef
Jeffery <jeffery@influx.io>
2018-10-24 19:29:19 +0800

    init

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -eu
 
+echo 'check env'
+soar=${soar:-default}
+
+if ! [[ -z "$soar" ]]; then
+  echo "请配置soar的环境变量"
+  exit 0
+fi
 app='.'
 echo -e '\033[33mstart git push check\033[0m'
 pip3 install -r requirments.txt
+
