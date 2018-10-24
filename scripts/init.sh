b027acc0f818528973a32e3371d69bde960abf28
Jeffery <jeffery@influx.io>
2018-10-24 19:41:16 +0800

    initial script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -eu
 
-echo 'check env'
-soar=${soar:-default}
+echo 'check install env'
 
-if ! [[ -z "$soar" ]]; then
-  echo "请配置soar的环境变量"
-  exit 0
+if [[ -z ${soar+x} ]]; then 
+	echo "请配置soar的环境变量:export soar=/usr/bin/soar";
+else 
+	echo "soar is set to '$soar'"; 
+	app='.'
+	echo -e '\033[33mstart git push check\033[0m'
+	pip3 install -r requirments.txt
 fi
-app='.'
-echo -e '\033[33mstart git push check\033[0m'
-pip3 install -r requirments.txt
+
+
 
