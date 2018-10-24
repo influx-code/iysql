d4d0340dc77d8255b710efc69d1aa8c9895390fd
Jeffery <jeffery@influx.io>
2018-10-24 11:26:28 +0800

    $1

---
+++ scripts/start.sh
@@ -0,0 +1,7 @@
+# @Author: Jeffery
+# @Date:   2018-10-24 11:20:25
+# @Last Modified by:   Jeffery
+# @Last Modified time: 2018-10-24 11:21:06
+set -eu
+echo -e 'start app server'
+python3 iysql/app.py
