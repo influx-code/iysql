3d154877d0c25db6a56d66531c5060764972ff56
ty4z2008 <ty4z2008@gmail.com>
2018-10-25 11:18:33 +0800

    修改初始化脚本

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,9 +10,11 @@ for cmd in ${NEEDED_COMMANDS} ; do
 		echo -e "\033[91m${cmd} is not set\033[0m"
 		exit 1
 	else
-		echo "${cmd} found"
-		app='.'
-		echo -e '\033[33mstart git push check\033[0m'
-		pip3 install -r requirments.txt
+		echo "${cmd} is install"
 	fi
 done
+
+app='.'
+echo -e '\033[33mstart git push check\033[0m'
+pip3 install -r requirments.txt
+exit 0
