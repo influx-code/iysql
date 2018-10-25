e5d8a5007af2a6e8eef1074a2e53157e0810975f
ty4z2008 <ty4z2008@gmail.com>
2018-10-25 11:20:25 +0800

    修改初始化脚本

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,18 +3,18 @@ set -eu
 
 echo 'Check install environment'
 
-NEEDED_COMMANDS="soar"
+NEEDED_COMMANDS="soar git"
 
 for cmd in ${NEEDED_COMMANDS} ; do
 	if ! command -v "${cmd}" &> /dev/null ; then
 		echo -e "\033[91m${cmd} is not set\033[0m"
 		exit 1
 	else
-		echo "${cmd} is install"
+		echo "${cmd} is install."
 	fi
 done
 
 app='.'
-echo -e '\033[33mstart git push check\033[0m'
+echo -e '\033[33mStart install python dependency\033[0m'
 pip3 install -r requirments.txt
 exit 0
