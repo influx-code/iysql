77c8289a38efe98c05f07b29bbf5701c3da4022d
ilcyb <hybmail1996@gmail.com>
2018-11-01 13:07:43 +0800

    tmp

--- iysql-go/handle/handle.go
+++ iysql-go/handle/handle.go
@@ -7,11 +7,24 @@ import (
 	"github.com/iysql/iysql-go/plugin"
 )
 
+type dbConnParam struct {
+	host     string
+	port     int
+	user     string
+	password string
+	database string
+}
+
+type fetchDatabasesParams struct {
+	datas dbConnParam
+}
+
 func SocketIoOnConnect(so socketio.Socket) {
 	log.Println("socket io connecting...")
 
 	so.On("get_types", getTypes)
 	so.On("sqladvisor", getSQLAdvisor)
+	so.On("fetch_database", fetchDatabases)
 	so.On("disconnection", func() {
 		log.Println("on disconnect")
 	})
@@ -37,6 +50,9 @@ func getSQLAdvisor(so socketio.Socket) {
 	so.Emit("sqladvisor.result", pluginResultMap)
 }
 
-func fetchDatabases(so socketio.Socket) {
-
+func fetchDatabases(so socketio.Socket, data dbConnParam) {
+	// var dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/information_schema",
+	// 	datas.user, datas.password, datas.host, datas.port)
+	log.Printf("hello %v %v", data, so.Request())
+	so.Emit("fetch_database.result", "hello")
 }
